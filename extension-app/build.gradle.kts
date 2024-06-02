17004fb8d62d4c260aec780c5f1100b5d808c50e
phj233 <2780990934@qq.com>
2024-06-02 23:51:59 +0800

    ⬆️ 更新`easybangumi-extension-api`

--- extension-app/build.gradle.kts
+++ extension-app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "top.phj233.easybangumi_extension_gugufan"
         minSdk = 24
         targetSdk = 34
-        versionCode = 6
+        versionCode = 7
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -33,13 +33,14 @@ android {
 }
 
 dependencies {
-    compileOnly("io.github.easybangumiorg:extension-api:1.6-SNAPSHOT")
+    compileOnly("io.github.easybangumiorg:extension-api:1.7-SNAPSHOT")
     implementation("org.jsoup:jsoup:1.17.2")
 
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("io.github.easybangumiorg:extension-api:1.7-SNAPSHOT")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
