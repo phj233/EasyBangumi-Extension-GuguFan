c75147ca9407be0d1b7c9d084cc93a684466abe5
phj233 <2780990934@qq.com>
2024-07-28 02:27:57 +0800

    🔧: 移除release签名配置

--- extension-app/build.gradle.kts
+++ extension-app/build.gradle.kts
@@ -45,7 +45,7 @@ android {
         release {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-            signingConfig = signingConfigs.getByName("release")
+//            signingConfig = signingConfigs.getByName("release")
         }
     }
     compileOptions {
